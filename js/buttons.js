2f809e7cc6718c56e5ca3a9af291a37472c84427
Gleb Shalabotov <gleb.shalabotov@forward.eu>
2022-05-07 11:26:36 +0200

    init-buttons: addEventListener to buttons

--- js/buttons.js
+++ js/buttons.js
@@ -1,3 +1,17 @@
 const addEventListenersToButtons = () => {
-    
+    initArrow({direction: 'left'});
+    initArrow({direction: 'right'});
+    initArrow({direction: 'up'});
+    initArrow({direction: 'down'});
+
+}
+
+const initArrow = ({direction}) => {
+    var directionClass = `arrow-${direction}`;
+    var $arrow = $(`div.${directionClass}`);
+    $arrow.on('click', arrowEvent);
+}
+
+const arrowEvent = (event) => {
+    console.log(event.currentTarget.dataset.direction);
 }
